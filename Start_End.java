465c317a80370194fa8b6a4c1fe17b9a547b7d36
dramyaa88 <58719475+dramyaa88@users.noreply.github.com>
2021-04-19 14:26:46 +0530

    Add files via upload

---
+++ Start_End.java
@@ -0,0 +1,17 @@
+package JourneyDetails;
+public class Start_End {
+	private String source;
+	private String destination;
+	public void setSource(String source) {
+  		this.source=source;
+  	  }
+  	  public void setDestination(String destination) {
+  		this.destination=destination;
+  	  }
+  	public String getSource() {
+		return source;
+	}
+	public String getDestination() {
+		return destination;
+	}
+}
---
+++ User_Validation.java
@@ -0,0 +1,12 @@
+package UserDetails;
+public class User_Validation {
+	private double otp;
+	private double min;
+	private double max;
+	public void setOtp(double otp) {
+		this.otp = otp;
+	}
+	public double getOtp() {
+		return otp;
+	}
+}
